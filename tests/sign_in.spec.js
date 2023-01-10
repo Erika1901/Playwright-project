2f43b5d290d9c36580fdabd5cb960e344c72a05d
Erika <egnaedzoa@zenity.fr>
2023-01-10 09:47:06 +0100

    ninethcommit

--- tests/sign_in.spec.js
+++ tests/sign_in.spec.js
@@ -5,11 +5,12 @@ test('test', async ({ page }) => {
   await page.goto('https://ztrain-web.vercel.app/auth/login');
   await page.getByRole('link', { name: 'S\'inscrire' }).click();
   await page.getByPlaceholder('Email').click();
-  await page.getByPlaceholder('Email').fill(login_data.login);
+  await page.getByPlaceholder('Email').fill(login_data.email);
   await page.locator('#password_register').click();
   await page.locator('#password_register').fill(login_data.password);
   await page.getByPlaceholder('Confirmer votre mot de passe').click();
   await page.getByPlaceholder('Confirmer votre mot de passe').fill('Malika2016+');
   await page.getByRole('button', { name: 'Inscription' }).click();
+  page.pause;
   await page.goto('https://ztrain-web.vercel.app/home');
 });
